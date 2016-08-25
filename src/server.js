011572d65c91be4eaa4ccd6c0a0f5e7dc84b3119
Joerg Domaschka <joerg.domaschka@uni-ulm.de>
2016-08-25 13:56:01 +0200

    made hostname configurable

--- src/server.js
+++ src/server.js
@@ -8,6 +8,7 @@ var dispatcher = require('httpdispatcher');
 
 //Lets define a port we want to listen to
 const PORT=8080; 
+const SERVERNAME="localhost"
 
 //We need a function which handles requests and send response
 function handleRequest(request, response){
@@ -213,7 +214,7 @@ var server = http.createServer(handleRequest);
 //Lets start our server
 server.listen(PORT, function(){
     //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+    console.log("Server listening on: http://%s:%s", SERVERNAME, PORT);
 });
 
 
